c51d88cd957976c4ef4e05d4f6e06cd47b60e0da
Codegass <wchweichenhao@gmail.com>
2019-07-29 06:25:46 -0400

    Test version of the service.

---
+++ twitter_credentials.py
@@ -0,0 +1,6 @@
+# Variables that contain the user credentials to access Twitter API.
+
+ACCESS_TOKEN = "3024663240-wGKl29fi0R4i4UcBirkX2d7RmgzBtTJ4IMjCD7t"
+ACCESS_TOKEN_SECRET = "Gsg9guf2f14oDzkltHj0NdIX0heJ1wt7DhBCZgaroBz26"
+CONSUMER_KEY = "n7D2vGs6Om8NVL1LlYxEHpkib"
+CONSUMER_SECRET = "hekaQgTEzeGxHXj52W8GDwlFyeQWjVsuTY24LPFUf0RQAFe4IO"
