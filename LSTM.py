3e52429147d1481953c35daee2dbb42626d47748
Codegass <wchweichenhao@gmail.com>
2019-08-23 08:39:39 -0400

    minor fix

--- LSTM.py
+++ LSTM.py
@@ -68,8 +68,8 @@ data = pd.read_csv('data.csv')
 data = data[['tweets','sentiment']]
 
 data = data[data.sentiment != 0]
-data['text'] = data['tweets'].apply(lambda x: x.lower())
-data['text'] = data['tweets'].apply((lambda x: re.sub('[^a-zA-z0-9\s]','',x)))
+data['tweets'] = data['tweets'].apply(lambda x: x.lower())
+data['tweets'] = data['tweets'].apply((lambda x: re.sub('[^a-zA-z0-9\s]','',x)))
 
 print(data[ data['sentiment'] == 1].size)
 print(data[ data['sentiment'] == -1].size)
@@ -79,8 +79,8 @@ for idx,row in data.iterrows():
     
 max_fatures = 2000
 tokenizer = Tokenizer(num_words=max_fatures, split=' ')
-tokenizer.fit_on_texts(data['text'].values)
-X = tokenizer.texts_to_sequences(data['text'].values)
+tokenizer.fit_on_texts(data['tweets'].values)
+X = tokenizer.texts_to_sequences(data['tweets'].values)
 X = pad_sequences(X)
 
 embed_dim = 128
@@ -108,9 +108,9 @@ X_validate = X_test[-validation_size:]
 Y_validate = Y_test[-validation_size:]
 X_test = X_test[:-validation_size]
 Y_test = Y_test[:-validation_size]
-score,acc = model.evaluate(X_test, Y_test, verbose = 2, batch_size = batch_size)
-print("score: %.2f" % (score))
-print("acc: %.2f" % (acc))
+# score,acc = model.evaluate(X_test, Y_test, verbose = 2, batch_size = batch_size)
+# print("score: %.2f" % (score))
+# print("acc: %.2f" % (acc))
 
 pos_cnt, neg_cnt, pos_correct, neg_correct = 0, 0, 0, 0
 for x in range(len(X_validate)):
@@ -137,7 +137,7 @@ twt = ['Pray for my parents friend, she went to go ring the all clear bell for c
 #vectorizing the tweet by the pre-fitted tokenizer instance
 twt = tokenizer.texts_to_sequences(twt)
 #padding the tweet to have exactly the same shape as `embedding_2` input
-twt = pad_sequences(twt, maxlen=28, dtype='int32', value=0)
+twt = pad_sequences(twt, maxlen=57, dtype='int32', value=0)
 print(twt)
 sentiment = model.predict(twt,batch_size=1,verbose = 2)[0]
 if(np.argmax(sentiment) == 0):
